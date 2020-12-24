99cb81d7a8e801a15af6e71f95e8c0c85eed6ba5
selkasse <selkasse@gmu.edu>
2020-12-24 02:18:33 -0500

    use gatsby-node to create blog post template pages

--- web/gatsby-node.js
+++ web/gatsby-node.js
@@ -1,15 +1,22 @@
-const {isFuture} = require('date-fns')
+/* eslint-disable space-before-function-paren */
+/* eslint-disable semi */
+/* eslint-disable object-curly-spacing */
+/* eslint-disable quotes */
+const { isFuture } = require("date-fns");
+const { format } = require("date-fns");
 /**
  * Implement Gatsby's Node APIs in this file.
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-async function createProjectPages (graphql, actions) {
-  const {createPage} = actions
+async function createProjectPages(graphql, actions) {
+  const { createPage } = actions;
   const result = await graphql(`
     {
-      allSanitySampleProject(filter: {slug: {current: {ne: null}}, publishedAt: {ne: null}}) {
+      allSanitySampleProject(
+        filter: { slug: { current: { ne: null } }, publishedAt: { ne: null } }
+      ) {
         edges {
           node {
             id
@@ -21,27 +28,64 @@ async function createProjectPages (graphql, actions) {
         }
       }
     }
-  `)
+  `);
 
-  if (result.errors) throw result.errors
+  if (result.errors) throw result.errors;
 
-  const projectEdges = (result.data.allSanitySampleProject || {}).edges || []
+  const projectEdges = (result.data.allSanitySampleProject || {}).edges || [];
 
   projectEdges
     .filter(edge => !isFuture(edge.node.publishedAt))
     .forEach(edge => {
-      const id = edge.node.id
-      const slug = edge.node.slug.current
-      const path = `/project/${slug}/`
+      const id = edge.node.id;
+      const slug = edge.node.slug.current;
+      const path = `/project/${slug}/`;
 
       createPage({
         path,
-        component: require.resolve('./src/templates/project.js'),
-        context: {id}
-      })
-    })
+        component: require.resolve("./src/templates/project.js"),
+        context: { id }
+      });
+    });
 }
 
-exports.createPages = async ({graphql, actions}) => {
-  await createProjectPages(graphql, actions)
+async function createBlogPostPages(graphql, actions) {
+  const { createPage } = actions;
+  const result = await graphql(`
+    {
+      allSanityPost(filter: { slug: { current: { ne: null } }, publishedAt: { ne: null } }) {
+        edges {
+          node {
+            id
+            publishedAt
+            slug {
+              current
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) throw result.errors;
+
+  const postEdges = (result.data.allSanityPost || {}).edges || [];
+
+  postEdges
+    .filter(edge => !isFuture(edge.node.publishedAt))
+    .forEach((edge, index) => {
+      const { id, slug = {}, publishedAt } = edge.node;
+      const dateSegment = format(publishedAt, "YYYY/MM");
+      const path = `/blog/${dateSegment}/${slug.current}/`;
+
+      createPage({
+        path,
+        component: require.resolve("./src/templates/blog-post.js"),
+        context: { id }
+      });
+    });
 }
+exports.createPages = async ({ graphql, actions }) => {
+  await createProjectPages(graphql, actions);
+  await createBlogPostPages(graphql, actions);
+};
--- web/src/lib/helpers.js
+++ web/src/lib/helpers.js
@@ -31,3 +31,17 @@ export function buildImageObj(source) {
 
   return imageObj;
 }
+
+export function toPlainText(blocks) {
+  if (!blocks) {
+    return "";
+  }
+  return blocks
+    .map(block => {
+      if (block._type !== "block" || !block.children) {
+        return "";
+      }
+      return block.children.map(child => child.text).join("");
+    })
+    .join("\n\n");
+}
