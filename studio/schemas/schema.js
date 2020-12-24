3c9a057b12bdc06c48ff7005f7b3bd4358057cea
selkasse <selkasse@gmu.edu>
2020-12-23 21:40:59 -0500

    import blog schema. confirmed data is getting to the front end

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -1,26 +1,34 @@
+/* eslint-disable quotes */
+/* eslint-disable semi */
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
+import createSchema from "part:@sanity/base/schema-creator";
 
 // Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
+import schemaTypes from "all:part:@sanity/base/schema-type";
 
 // Document types
-import category from './documents/category'
-import person from './documents/person'
-import sampleProject from './documents/sampleProject'
-import siteSettings from './documents/siteSettings'
+import category from "./documents/category";
+import person from "./documents/person";
+import sampleProject from "./documents/sampleProject";
+import siteSettings from "./documents/siteSettings";
+import post from "./documents/post";
+import author from "./documents/author";
 
 // Object types
-import bioPortableText from './objects/bioPortableText'
-import figure from './objects/figure'
-import projectMember from './objects/projectMember'
-import projectPortableText from './objects/projectPortableText'
-import simplePortableText from './objects/simplePortableText'
+import bioPortableText from "./objects/bioPortableText";
+import figure from "./objects/figure";
+import projectMember from "./objects/projectMember";
+import projectPortableText from "./objects/projectPortableText";
+import simplePortableText from "./objects/simplePortableText";
+import mainImage from "./objects/mainImage";
+import excerptPortableText from "./objects/excerptPortableText";
+import bodyPortableText from "./objects/bodyPortableText";
+import authorReference from "./objects/authorReference";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
-  name: 'portfolio',
+  name: "portfolio",
   // Then proceed to concatenate our our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
@@ -31,11 +39,17 @@ export default createSchema({
     projectMember,
     projectPortableText,
     simplePortableText,
+    mainImage,
+    excerptPortableText,
+    bodyPortableText,
+    authorReference,
     // The following are document types which will appear
     // in the studio.
     category,
     person,
     sampleProject,
-    siteSettings
+    siteSettings,
+    post,
+    author
   ])
-})
+});
--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,27 +1,50 @@
-import React from 'react'
-import {graphql} from 'gatsby'
+/* eslint-disable object-curly-spacing */
+/* eslint-disable semi */
+import React from "react";
+import { graphql } from "gatsby";
 import {
   mapEdgesToNodes,
   filterOutDocsWithoutSlugs,
   filterOutDocsPublishedInTheFuture
-} from '../lib/helpers'
-import Container from '../components/container'
-import GraphQLErrorList from '../components/graphql-error-list'
-import ProjectPreviewGrid from '../components/project-preview-grid'
-import SEO from '../components/seo'
-import Layout from '../containers/layout'
+} from "../lib/helpers";
+import Container from "../components/container";
+import GraphQLErrorList from "../components/graphql-error-list";
+import ProjectPreviewGrid from "../components/project-preview-grid";
+import SEO from "../components/seo";
+import Layout from "../containers/layout";
 
 export const query = graphql`
+  fragment SanityImage on SanityMainImage {
+    crop {
+      _key
+      _type
+      top
+      bottom
+      left
+      right
+    }
+    hotspot {
+      _key
+      _type
+      x
+      y
+      height
+      width
+    }
+    asset {
+      _id
+    }
+  }
   query IndexPageQuery {
-    site: sanitySiteSettings(_id: {regex: "/(drafts.|)siteSettings/"}) {
+    site: sanitySiteSettings(_id: { regex: "/(drafts.|)siteSettings/" }) {
       title
       description
       keywords
     }
     projects: allSanitySampleProject(
       limit: 6
-      sort: {fields: [publishedAt], order: DESC}
-      filter: {slug: {current: {ne: null}}, publishedAt: {ne: null}}
+      sort: { fields: [publishedAt], order: DESC }
+      filter: { slug: { current: { ne: null } }, publishedAt: { ne: null } }
     ) {
       edges {
         node {
@@ -56,31 +79,54 @@ export const query = graphql`
         }
       }
     }
+    posts: allSanityPost(
+      limit: 6
+      sort: { fields: [publishedAt], order: DESC }
+      filter: { slug: { current: { ne: null } }, publishedAt: { ne: null } }
+    ) {
+      edges {
+        node {
+          id
+          publishedAt
+          mainImage {
+            ...SanityImage
+            alt
+          }
+          title
+          _rawExcerpt
+          slug {
+            current
+          }
+        }
+      }
+    }
   }
-`
+`;
 
 const IndexPage = props => {
-  const {data, errors} = props
+  const { data, errors } = props;
+
+  console.log(data.posts);
 
   if (errors) {
     return (
       <Layout>
         <GraphQLErrorList errors={errors} />
       </Layout>
-    )
+    );
   }
 
-  const site = (data || {}).site
+  const site = (data || {}).site;
   const projectNodes = (data || {}).projects
     ? mapEdgesToNodes(data.projects)
-      .filter(filterOutDocsWithoutSlugs)
-      .filter(filterOutDocsPublishedInTheFuture)
-    : []
+        .filter(filterOutDocsWithoutSlugs)
+        .filter(filterOutDocsPublishedInTheFuture)
+    : [];
 
   if (!site) {
     throw new Error(
       'Missing "Site settings". Open the studio at http://localhost:3333 and add some content to "Site settings" and restart the development server.'
-    )
+    );
   }
 
   return (
@@ -90,14 +136,14 @@ const IndexPage = props => {
         <h1 hidden>Welcome to {site.title}</h1>
         {projectNodes && (
           <ProjectPreviewGrid
-            title='Latest projects'
+            title="Latest projects"
             nodes={projectNodes}
-            browseMoreHref='/archive/'
+            browseMoreHref="/archive/"
           />
         )}
       </Container>
     </Layout>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
